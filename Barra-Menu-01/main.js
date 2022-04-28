a19aacee881f6b30335cf308d7f920a10a0346ec
CleitonJCosta <Cleitonjc94@hotmail.com>
2022-04-27 23:08:24 -0300

    Atualizando

---
+++ Barra-Menu-01/main.js
@@ -0,0 +1,8 @@
+// testando o link do javascript com html com tent
+ function openNav(){
+    document.getElementById('myNav').style.width="100%";
+    // alert("TEST")
+ }
+ function closeNav(){
+    document.getElementById('myNav').style.width = '0';
+ }
